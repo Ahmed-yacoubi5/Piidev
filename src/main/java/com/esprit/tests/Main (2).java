123a5d339986e384bc43e5266c46ebd01f769e06
Ahmed-yacoubi5 <ahmedchihi00@gmail.com>
2025-03-04 10:03:22 +0100

    ..

---
+++ src/main/java/com/esprit/tests/Main (2).java
@@ -0,0 +1,29 @@
+package com.esprit.tests;
+
+
+import com.esprit.models.RetourEvenement;
+
+
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+      /*  RetourEvenementService res = new RetourEvenementService();
+
+
+        RetourEvenement retour1 = new RetourEvenement(1, 2, "Super événement, bien organisé !","d", 5);
+        res.ajouter(retour1);
+
+
+        RetourEvenement retourModifie = new RetourEvenement(1, 1, 2, "Événement intéressant mais trop long", 4, LocalDateTime.now());
+        res.modifier(retourModifie);
+
+
+        res.supprimer(1);
+
+        System.out.println("Liste des retours : " + res.rechercher());*/
+    }
+}
+
+
+
